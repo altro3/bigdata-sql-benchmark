1a441387485b532794f13cf946daf6d04020a0bb
twx1268413 <trofimov.alexey@huawei-partners.com>
2023-08-16 14:17:56 +0700

    Scipts fixes

--- spark-hudi/src/main/scala/spark3/sql/CliOptions.scala
+++ spark-hudi/src/main/scala/spark3/sql/CliOptions.scala
@@ -4,6 +4,6 @@ import com.beust.jcommander.Parameter
 import java.io.Serializable
 
 class CliOptions extends Serializable {
-  @Parameter(names = Array(Array("-q", "--queries")), description = "sql query names. If the value is 'all', all queries will be executed.", required = true) var queries: String = null
-  @Parameter(names = Array(Array("-d", "--database")), description = "sql query names. If the value is 'all', all queries will be executed.", required = true) var database: String = null
+  @Parameter(names = Array("-q", "--queries"), description = "sql query names. If the value is 'all', all queries will be executed.", required = true) var queries: String = null
+  @Parameter(names = Array("-d", "--database"), description = "sql query names. If the value is 'all', all queries will be executed.", required = true) var database: String = null
 }
